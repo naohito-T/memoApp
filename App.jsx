e190d0a4f462c741f7819a8af0e14c76fa5ac68e
naohito-T <naohito.tanaka0523@gmail.com>
2021-02-04 10:06:30 +0900

    Create LogInScreen

--- App.jsx
+++ App.jsx
@@ -4,10 +4,11 @@ import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
 import MemoEditScreen from './src/screens/MemoEditScreen';
 import MemoCreateScreen from './src/screens/MemoCreateScreen';
+import LogInScreen from './src/screens/LogInScreen';
 
 export default function App() {
   return (
     // JSXゾーン
-    <MemoEditScreen />
+    <LogInScreen />
   );
 }

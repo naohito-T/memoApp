b4e774cc56efd7366759e1a6190a8fbb35c337e5
naohito-T <naohito.tanaka0523@gmail.com>
2021-02-04 10:19:10 +0900

    create jsxScreens

--- App.jsx
+++ App.jsx
@@ -5,10 +5,11 @@ import MemoDetailScreen from './src/screens/MemoDetailScreen';
 import MemoEditScreen from './src/screens/MemoEditScreen';
 import MemoCreateScreen from './src/screens/MemoCreateScreen';
 import LogInScreen from './src/screens/LogInScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
 
 export default function App() {
   return (
     // JSXゾーン
-    <LogInScreen />
+    <SignUpScreen />
   );
 }
